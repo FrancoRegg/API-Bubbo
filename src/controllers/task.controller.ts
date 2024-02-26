b6d32ad31f774cfbc5fdca87abafd40ec7ba6dee
FrancoRegg <francoreggiardo97@gmail.com>
2024-02-26 10:41:22 +0100

    Se añade un capturador de errores

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,55 +1,54 @@
-import { Request, Response, request, } from 'express'
-import { handleHttp } from '../utils/error.handle'
-import { insertTask, showTasks, showTask, modifyTask, hideTask } from '../services/task.service'
-
+import { Request, Response } from 'express';
+import { handleHttp } from '../utils/error.handle';
+import { insertTask, showTasks, showTask, modifyTask, hideTask } from '../services/task.service';
 
 const getTasks = async (req: Request, res: Response) => {
-    try{
-        const response = await showTasks()
-        res.send(response)
-    }catch (e) {
-        handleHttp( res, 'ERROR_GET_TASKS', )
+    try {
+        const response = await showTasks();
+        res.send(response);
+    } catch (e) {
+        handleHttp(res, 'ERROR_GET_TASKS', e);
     }
-}
+};
 
-const getTask = async ({params}: Request, res: Response) => {
-    try{
-        const {id} = params
-        const response = await showTask(id)
-        const data = response ? response : 'NOT_FOUND'
-        res.send(data)
-    }catch (e) {
-        handleHttp( res, 'ERROR_GET_TASK' )
+const getTask = async ({ params }: Request, res: Response) => {
+    try {
+        const { id } = params;
+        const response = await showTask(id);
+        const data = response ? response : 'NOT_FOUND';
+        res.send(data);
+    } catch (e) {
+        handleHttp(res, 'ERROR_GET_TASK', e);
     }
-}
+};
 
-const updateTask = async ({params, body}: Request, res: Response) => {
-    try{
-        const {id} = params
-        const response = await modifyTask(id, body)
-        res.send(response)
-    }catch (e) {
-        handleHttp( res, 'ERROR_UPDATE_TASK' )
+const updateTask = async ({ params, body }: Request, res: Response) => {
+    try {
+        const { id } = params;
+        const response = await modifyTask(id, body);
+        res.send(response);
+    } catch (e) {
+        handleHttp(res, 'ERROR_UPDATE_TASK', e);
     }
-}
+};
 
 const postTask = async (req: Request, res: Response) => {
-    try{
-        const responseTask = await insertTask(req.body)
-        res.send(responseTask)
-    }catch (e) {
-        handleHttp( res, 'ERROR_POST_TASK', e )
+    try {
+        const responseTask = await insertTask(req.body);
+        res.send(responseTask);
+    } catch (e) {
+        handleHttp(res, 'ERROR_POST_TASK', e);
     }
-}
+};
 
-const deleteTask = async ({params}: Request, res: Response) => {
-    try{
-        const {id} = params
-        const response = await hideTask(id)
-        res.send(response)
-    }catch (e) {
-        handleHttp( res, 'ERROR_DELETE_TASK' )
+const deleteTask = async ({ params }: Request, res: Response) => {
+    try {
+        const { id } = params;
+        const response = await hideTask(id);
+        res.send(response);
+    } catch (e) {
+        handleHttp(res, 'ERROR_DELETE_TASK', e);
     }
-}
+};
 
-export { getTasks, getTask, updateTask, postTask, deleteTask }
+export { getTasks, getTask, updateTask, postTask, deleteTask };
