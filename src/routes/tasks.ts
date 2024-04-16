c4b72766d1c2de8deeeba5c758d8b4369283e0f4
FrancoRegg <francoreggiardo97@gmail.com>
2024-04-16 18:59:03 +0200

    Elimino middleware que no utilizo

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import { getAllTasks, getOneTask, updateOneTask, createNewTask, deleteOneTask } from '../controllers/task.controller';
-import { logMiddleware } from '../middleware/log';
+
 
 const router = Router();
 
 
 router.get('/', getAllTasks)
-router.get('/:id', getOneTask, logMiddleware)
+router.get('/:id', getOneTask)
 router.put('/:id', updateOneTask)
 router.post('/', createNewTask)
 router.delete('/:id', deleteOneTask)
