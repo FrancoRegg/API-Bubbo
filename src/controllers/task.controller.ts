daee0d52a17b0e721cc01fecf02464883948184a
FrancoRegg <francoreggiardo97@gmail.com>
2024-04-18 19:36:23 +0200

    Prueba nueva funcion para manejar errores

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -5,52 +5,53 @@ import { insertTask, showTasks, showTask, modifyTask, hideTask } from '../servic
 
 
 const getAllTasks = async (req: Request, res: Response) => {
-    try {
-        const response = await showTasks();
-        res.send(response) 
-    } catch (e) {
-        handleHttp(res, 'ERROR_GET_TASKS', e);
-    }
+  try {
+    const response = await showTasks();
+    res.send(response)
+  } catch (e) {
+    //handleHttp(res, 'ERROR_GET_TASKS', e);
+
+  }
 };
 
 const getOneTask = async ({ params }: Request, res: Response) => {
-    try {
-        const { id } = params;
-        const response = await showTask(id);
-        const data = response ? response : 'NOT_FOUND';
-        res.send(data);
-    } catch (e) {
-        handleHttp(res, 'ERROR_GET_TASK', e);
-    }
+  try {
+    const { id } = params;
+    const response = await showTask(id);
+    //const data = response ? response : 'NOT_FOUND';
+    res.send(response);
+  } catch (e) {
+    //handleHttp(res, 'ERROR_GET_TASK', e);
+  }
 };
 
 const updateOneTask = async ({ params, body }: Request, res: Response) => {
-    try {
-        const { id } = params;
-        const response = await modifyTask(id, body);
-        res.send(response);
-    } catch (e) {
-        handleHttp(res, 'ERROR_UPDATE_TASK', e);
-    }
+  try {
+    const { id } = params;
+    const response = await modifyTask(id, body);
+    res.send(response);
+  } catch (e) {
+    //handleHttp(res, 'ERROR_UPDATE_TASK', e);
+  }
 };
 
 const createNewTask = async (req: Request, res: Response) => {
-    try {
-        const responseTask = await insertTask(req.body);
-        res.send(responseTask);
-    } catch (e) {
-        handleHttp(res, 'ERROR_POST_TASK', e);
-    }
+  try {
+    const responseTask = await insertTask(req.body);
+    res.send(responseTask);
+  } catch (e) {
+    handleHttp(res, 'ERROR_POST_TASK', e);
+  }
 };
 
 const deleteOneTask = async ({ params }: Request, res: Response) => {
-    try {
-        const { id } = params;
-        const response = await hideTask(id);
-        res.send(response);
-    } catch (e) {
-        handleHttp(res, 'ERROR_DELETE_TASK', e);
-    }
+  try {
+    const { id } = params;
+    const response = await hideTask(id);
+    res.send(response);
+  } catch (e) {
+    //handleHttp(res, 'ERROR_DELETE_TASK', e);
+  }
 };
 
 
